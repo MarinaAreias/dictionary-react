11383cba6267f741f5b1bce14900bb5f1f8fabd9
MarinaAreias <marinasareias@gmail.com>
2021-04-10 20:13:22 +0200

    material ui
    -themeProvider and Paper
    -created dark/light mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
+import React, { useState } from "react";
 import "./App.css";
 import Dictionary from "./Dictionary";
-import Button from "./Button";
-import Footer from "./Footer";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { Switch, Paper } from "@material-ui/core";
 
 export default function App(props) {
+  // set theme
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light",
+    },
+  });
+
+  const handleDarkMode = function () {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="app">
-      <div className="container">
-        <header className="header">
-          <h1>Your Dictionary</h1>
-          <Button />
-        </header>
-        <main>
-          <Dictionary />
-        </main>
-        <footer>
-          <Footer />
-        </footer>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ minHeight: "100vh" }}>
+        <div className="app">
+          <div className="container">
+            <header className="header text-center">
+              <ul>
+                <li>
+                  <h1>Your Dictionary</h1>
+                </li>
+                <li className="ms-5">
+                  <Switch onChange={handleDarkMode} value={darkMode} />
+                </li>
+              </ul>
+            </header>
+            <main>
+              <Dictionary />
+            </main>
+            <footer className="text-center">Created by Marina Areias</footer>
+          </div>
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
